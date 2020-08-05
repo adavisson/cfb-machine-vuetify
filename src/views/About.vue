a7003bc2229fc401f77a097bde151d0aa9538d7c
Andrew Davisson <akdavisson4@gmail.com>
2020-08-05 14:53:41 -0600

    added some styling

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="d-flex-md about">
+    <h1 class="text-h1 title">About</h1>
+    <div class="content">
+      <p>This is an about page</p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {},
+}
+</script>
+
+<style scoped>
+  .about {
+    flex-direction: column;
+  }
+  .title {
+    text-align: center;
+  }
+  .content {
+    width: 80%;
+    margin: auto;
+    text-align: center;
+  }
+</style>
+
