48a408e87e038238d54741792a8cb69cc34dd70a
Andrew Davisson <akdavisson4@gmail.com>
2020-08-05 12:39:19 -0600

    added theming

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,21 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.lightGreen.base,
+        secondary: colors.orange.base,
+      },
+      dark: {
+        primary: colors.lightGreen.base,
+        secondary: colors.orange.base,
+      }
+    },
+    dark: true,
+  },
+});
