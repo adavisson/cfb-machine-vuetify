d6557c2e982f143c68f13193e1859ef505e95446
Andrew Davisson <akdavisson4@gmail.com>
2020-08-05 14:53:32 -0600

    use router

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <v-app>
     <AppBar />
     <v-main>
-      <Home />
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Home from "./views/Home";
+// import Home from "./views/Home";
 import AppBar from "./components/AppBar";
 
 export default {
   name: "App",
 
   components: {
-    Home,
+    // Home,
     AppBar,
   },
 
