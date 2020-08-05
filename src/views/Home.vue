94aa47ecf74da66d3ef1d1bfd9247ad22e28f7f0
Andrew Davisson <akdavisson4@gmail.com>
2020-08-05 15:39:35 -0600

    changed title and content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="d-flex-md home">
-    <h1 class="text-h1 title">Home</h1>
+    <h1 class="text-h1 header">CFB Machine</h1>
     <div class="content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida metus non placerat posuere. Fusce quis erat massa. Donec augue massa, pharetra non felis et, rhoncus aliquet ligula. Nam ut dui massa. Quisque pellentesque sapien id massa pellentesque mattis quis in velit. Nunc blandit lectus quis quam commodo, in blandit metus molestie. Phasellus ultrices nibh et nunc aliquam, sed commodo justo lobortis. Aenean in diam id tellus iaculis condimentum in sed lacus. Aenean condimentum nunc nunc, nec pulvinar purus posuere et. Quisque luctus turpis ac ullamcorper venenatis. Phasellus et ex venenatis, venenatis nisi eget, porttitor dui. Suspendisse ultricies diam id sapien accumsan tempor.</p>
-      <p>Morbi tristique lorem ac lectus suscipit interdum iaculis eu purus. Duis malesuada elementum justo, ut ornare mauris suscipit a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer vitae congue sapien, non mattis elit. Vivamus pellentesque dolor vel turpis interdum pulvinar. Sed quis magna eget tellus commodo pharetra. Mauris elementum accumsan augue et lobortis.</p>
-      <p>Morbi euismod aliquam nisi, eu pellentesque lorem vestibulum a. Fusce non dolor non enim viverra fermentum. Sed quis cursus justo, quis faucibus sem. In varius tellus lectus, ut hendrerit augue lacinia aliquam. Aenean pulvinar augue ligula, vel aliquam augue scelerisque faucibus. Quisque leo erat, feugiat at congue ac, rutrum quis tortor. In pellentesque erat vel augue molestie, non fermentum nulla lobortis. Duis commodo elit ex, sed fringilla diam vulputate id. Pellentesque iaculis, velit quis consectetur hendrerit, mi leo porttitor nunc, et ullamcorper neque quam vulputate lectus. Ut at massa sagittis, viverra justo et, varius massa. Sed non vulputate est, nec varius lectus. Donec consequat lorem quis ultrices vestibulum. Nulla convallis orci in luctus finibus. Sed laoreet ut magna vel cursus. Suspendisse nisi magna, semper a ullamcorper vulputate, volutpat ac libero.</p>
-      <p>Cras ex neque, ultricies eget ultricies eget, mollis nec dui. Ut blandit fringilla facilisis. Nulla lectus odio, tristique ac nibh eget, venenatis faucibus urna. Vestibulum pellentesque tristique erat sodales varius. Duis quis vulputate tortor. Cras tristique arcu tellus, et semper purus ultricies nec. Cras vel urna metus. Suspendisse justo nisi, pharetra imperdiet turpis nec, feugiat tempor tellus. Duis quis pellentesque lacus, eget pretium metus. Suspendisse ut pharetra dolor. Curabitur non condimentum nisl, at tempus leo. Donec vitae nibh tempus, sollicitudin nisl eget, dictum ex. Morbi et nisi vitae ex suscipit fermentum. Fusce quis lobortis sem, vel pulvinar quam. Donec aliquet lectus elit, blandit vestibulum metus fringilla sed. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-      <p>Etiam consectetur, lorem et varius lobortis, nisi sapien varius metus, vitae gravida dolor dolor in est. Etiam pulvinar tellus et consequat ultrices. Nulla quis nisl id purus mollis cursus et a ex. In hac habitasse platea dictumst. Etiam in posuere lorem. Suspendisse risus mi, placerat vel velit eget, volutpat hendrerit quam. Nam sit amet gravida ante. Nulla lacinia tristique mauris finibus interdum. Maecenas eu efficitur tellus. Mauris ut mi odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus ullamcorper egestas.</p>
-      <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam et tellus lacinia justo consequat blandit. Sed ac augue sit amet lorem ornare vulputate. Nulla ut accumsan nunc, eget pharetra leo. Fusce ut ex a elit finibus maximus sit amet aliquam dui. Donec laoreet bibendum odio sed posuere. Nunc nec sollicitudin ipsum. Donec consequat, nunc eu vehicula finibus, magna magna ullamcorper nisi, at vestibulum ligula velit eget dolor. Morbi nec enim sit amet lectus placerat tristique sagittis eget ligula. Curabitur egestas, nisl vel interdum interdum, odio augue sodales mauris, sit amet semper massa nibh vitae diam. Vivamus gravida diam ac aliquet volutpat. Vestibulum faucibus convallis mi nec fringilla. Morbi id ex venenatis, laoreet diam vel, vestibulum nisi. Praesent condimentum ante augue, sed gravida erat feugiat eu.</p>
+      <p>Welcome to the CFB Machine. Explore this site to find historical college football stats.</p>
     </div>
   </div>
 </template>
@@ -25,12 +20,14 @@ export default {
     align-items: center;
     margin: auto;
   }
-  .title {
+  .header {
     text-align: center;
+    padding-bottom: .5em;
+    padding-top: .2em;
   }
   .content {
     width: 80%;
     margin: auto;
-    text-align: justify;
+    text-align: center;
   }
 </style>
