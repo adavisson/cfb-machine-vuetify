fdc10fd1791042872e27cedbca33828f2382cdbe
Andrew Davisson <akdavisson4@gmail.com>
2020-08-05 11:23:41 -0600

    Started navbar component

---
+++ src/components/AppBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="navigation">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+    </v-navigation-drawer>
+    <v-app-bar app color="green" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>CFB Machine</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+  data: () => ({
+    drawer: false,
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
