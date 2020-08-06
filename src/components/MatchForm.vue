d91a12e51bea74f929e41e51416b310e41f3c3a7
Andrew Davisson <akdavisson4@gmail.com>
2020-08-05 23:16:37 -0600

    made form

---
+++ src/components/MatchForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-form class="form">
+    <v-select
+      :items="teams"
+      label="Team 1"
+      color="secondary"
+      dense
+    ></v-select>
+    <v-select
+      :items="teams"
+      label="Team 2"
+      color="secondary"
+      dense
+    ></v-select>
+    <p>{{firstTeam}}</p>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "MatchForm",
+  data: () => ({
+    teams: ["Team1", "Team2", "Team3"],
+    firstTeam: "",
+    secondTeam: ""
+  }),
+}
+</script>
+
+<style scoped>
+  .form {
+    width: 60%;
+    margin: auto;
+  }
+</style>
