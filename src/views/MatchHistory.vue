2a620ba38b361a8643f9506d27ebbf16a19b0677
Andrew Davisson <akdavisson4@gmail.com>
2020-08-05 22:35:28 -0600

    created match history view

---
+++ src/views/MatchHistory.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="d-flex-md match-history">
+    <h1 class="text-md-h1 header">Match History</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MatchHistory',
+  data:() => ({}),
+}
+</script>
+
+<style scoped>
+  .match-history {
+    flex-direction: column;
+  }
+  .header {
+    text-align: center;
+    padding-bottom: 0.5em;
+    padding-top: 0.2em;
+  }
+</style>
