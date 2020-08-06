a0f4cbd5903a0607c3a852fa1da15613a217f484
Andrew Davisson <akdavisson4@gmail.com>
2020-08-06 08:44:24 -0600

    created match results component

---
+++ src/components/MatchResults.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Results</p>
+    <v-btn color="secondary" @click="handleClick">Pick Different Teams</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MatchResults",
+  data: () => ({}),
+  props: ['handleSubmit'],
+  methods: {
+    handleClick() {
+      this.$emit('handleSubmit')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
