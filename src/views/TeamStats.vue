f4da3fe791df712bf1493ed676bb8586954d9378
Andrew Davisson <akdavisson4@gmail.com>
2020-08-07 07:43:55 -0600

    imported Form and results components

--- src/views/TeamStats.vue
+++ src/views/TeamStats.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="d-flex-md team-stats">
     <h1 class="text-md-h1 header">Team Statistics</h1>
+    <TeamStatsForm v-if="!submitted" />
+    <TeamStatsResults v-else />
   </div>
 </template>
 
 <script>
+import TeamStatsForm from "../components/TeamStatsForm"
+import TeamStatsResults from "../components/TeamStatsResults"
+
 export default {
   name: 'TeamStats',
-  data: () => ({}),
+  components: {
+    TeamStatsForm,
+    TeamStatsResults
+  },
+  data: () => ({
+    submitted: false
+  }),
 }
 </script>
 
