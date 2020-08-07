fcf728a0bb5eefba47ba5f7ba0344182e4761dcc
Andrew Davisson <akdavisson4@gmail.com>
2020-08-07 07:41:07 -0600

    created form and results components for teamstats

---
+++ src/components/TeamStatsForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Team Statistics</h2>
+</template>
+
+<script>
+export default {
+  name: "TeamStatsForm",
+  data: () => ({}),
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TeamStatsResults.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Team Statistics Results</h2>
+</template>
+
+<script>
+export default {
+  name: "TeamStatsResults",
+  data: () => ({}),
+}
+</script>
+
+<style scoped>
+
+</style>
