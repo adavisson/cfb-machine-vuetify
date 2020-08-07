1f782b02048982e70aa8835b52858e1ced2688b5
Andrew Davisson <akdavisson4@gmail.com>
2020-08-07 08:00:34 -0600

    added form and filled in teams and years

--- src/components/TeamStatsForm.vue
+++ src/components/TeamStatsForm.vue
@@ -1,14 +1,54 @@
 <template>
-  <h2>Team Statistics</h2>
+  <v-form @submit.prevent="submitForm" class="form" id="team-stats-form">
+    <v-select
+      :items="teams"
+      item-text="school"
+      item-value="school"
+      label="Team"
+      color="secondary"
+      v-model="selectedTeam"
+      dense
+    ></v-select>
+    <v-select
+      :items="years"
+      item-text="text"
+      item-value="value"
+      label="Year"
+      color="secondary"
+      v-model="year"
+      dense
+    ></v-select>
+    <v-btn type="submit" color="secondary" form="team-stats-form">Submit</v-btn>
+  </v-form>
 </template>
 
 <script>
 export default {
   name: "TeamStatsForm",
-  data: () => ({}),
+  props: ['teams', 'handleSubmit'],
+  data: () => ({
+    selectedTeam: "",
+    years: [],
+    year: ""
+  }),
+  beforeMount() {
+    this.fillYears()
+  },
+  methods: {
+    fillYears() {
+      let thisYear = new Date().getFullYear() 
+      while (thisYear >=  1900) {
+        this.years.push({ text: thisYear, value: thisYear })
+        thisYear--
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .form {
+    width: 60%;
+    margin: auto;
+  }
 </style>
