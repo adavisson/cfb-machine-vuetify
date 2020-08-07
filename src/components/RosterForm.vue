70c84670a7542b4d9e62c35d36e114542550b27c
Andrew Davisson <akdavisson4@gmail.com>
2020-08-06 23:13:21 -0600

    fetched teams

--- src/components/RosterForm.vue
+++ src/components/RosterForm.vue
@@ -1,14 +1,31 @@
 <template>
-  <h2>Form</h2>
+  <v-form @submit.prevent="submitForm" class="form" id="roster-form">
+    <v-select
+      :items="teams"
+      item-text="school"
+      item-value="school"
+      label="Team"
+      color="secondary"
+      v-model="team"
+      dense
+    ></v-select>
+    <v-btn type="submit" color="secondary" form="roster-form">Submit</v-btn>
+  </v-form>
 </template>
 
 <script>
 export default {
   name: "RosterForm",
-  data: () => ({}),
+  data: () => ({
+    team: ""
+  }),
+  props: ["teams"]
 }
 </script>
 
 <style scoped>
-
+  .form {
+    width: 60%;
+    margin: auto;
+  }
 </style>
--- src/views/Roster.vue
+++ src/views/Roster.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex-md roster">
     <h1 class="text-md-h1 header">Roster</h1>
-    <RosterForm v-if="!submitted"/>
+    <RosterForm v-if="!submitted" :teams="teams" />
     <RosterResults v-else />
   </div>
 </template>
@@ -17,8 +17,23 @@ export default {
     RosterResults
   },
   data: () => ({
-    submitted: false
-  })
+    submitted: false,
+    teams: []
+  }),
+  beforeMount() {
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const result = await fetch('https://api.collegefootballdata.com/teams/fbs');
+        const data = await result.json();
+        this.teams = data;
+      } catch (e) {
+        console.log(e)
+      }
+    },
+  }
 };
 </script>
 
