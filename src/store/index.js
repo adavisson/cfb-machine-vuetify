c66fea031b5d2df64004f34ec98c6a60d2c04d3d
Andrew Davisson <akdavisson4@gmail.com>
2020-08-19 10:20:21 -0600

    added teams state and fetchTeams mutation

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,25 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    teams: []
+  },
+  mutations: {
+    fetchTeams(state) {
+      const fetchData = async() => {
+        try {
+          const result = await fetch(
+            "https://api.collegefootballdata.com/teams/fbs"
+          );
+          const data = await result.json();
+          state.teams = data;
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      fetchData()
+    },
+  },
   actions: {},
   modules: {}
 });
