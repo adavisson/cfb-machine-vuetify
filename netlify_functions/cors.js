df64786090fe69a6d60a840766e0765c62433398
Andrew Davisson <akdavisson4@gmail.com>
2021-09-10 13:15:52 -0600

    another shot

---
+++ netlify_functions/cors.js
@@ -0,0 +1,37 @@
+exports.handler = async (event, context) => {
+  let url = event.path.split('.netlify/functions/cors/')[1];
+  url = decodeURIComponent(url);
+  url = new URL(url);
+
+  for (let i in event.queryStringParameters) {
+    url.searchParams.append(i, event.queryStringParameters[i]);
+  }
+
+  const headerToSend = {
+    Authorization: event.headers['Authorization'] || '',
+    content_type: 'application/json',
+    host: url.host,
+  };
+
+  const options = {
+    method: event.httpMethod.toUpperCase(),
+    headers: headerToSend,
+    body: event.body,
+  };
+
+  if (['GET', 'HEAD'].includes(event.httpMethod.toUpperCase())) {
+    delete options.body;
+  }
+
+  const resp = await fetch(url, options);
+  const resp_json = resp.json();
+  const headers = resp.headers.raw();
+
+  return {
+    statusCode: 200,
+    body: resp_json,
+    headers: {
+      content_type: String(headers['content_type']) || 'application/json',
+    },
+  };
+};
--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,7 @@ export default new Vuex.Store({
       const fetchData = async () => {
         try {
           const result = await fetch(
-            '/cors-proxy/https://api.collegefootballdata.com/teams/fbs',
+            '/.netlify/function/cors/https://api.collegefootballdata.com/teams/fbs',
             {
               headers: {
                 Authorization: 'Bearer ' + process.env.VUE_APP_API_KEY,
